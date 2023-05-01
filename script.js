ad25fffc24d0f4afd787b46cffc20f49888a14f9
tgao17 <113127230+tgao17@users.noreply.github.com>
2023-05-01 16:38:20 -0400

    added buttons

--- script.js
+++ script.js
@@ -12,38 +12,83 @@ function userChoiceTest(userChoice) {
   return userChoice;
 }
 
-function play(player, computer){
+function play(player, computer){``
   if((player === "rock" && computer === "scissors") || (player === "scissors" && computer === "paper") || (player === "paper" && computer === "rock")){
-    return true;
+    currentScore.textContent ++;
+    yourScore++;
+    return `You Win! ${player} beats ${computer}`;
+  } else if (player === computer){
+    return `Tie!`
   }
-  return false;
+  pcScore ++;
+  currentComputerScore.textContent ++;
+  return `You Lose! ${computer} beats ${player}`;;
 }
 
-function game(){
-  let score = 0;
-  for(let i = 0; i<5; i++){
-    let userChoice = prompt("pick rock, paper, or scissors"); // do i need this?
-    userChoice = userChoice.toLowerCase();
-    userChoiceTest(userChoice);
-    let computerChoice = getComputerChoice()
-    if(play(userChoice, computerChoice)){
-      console.log(`You Win! ${userChoice} beats ${computerChoice}`);
-      score ++;
+// event listeners for ui
+const choiceButtons = document.querySelectorAll('.btnBlocks button');
+choiceButtons.forEach((item) =>{
+  item.addEventListener('mouseover', () =>{
+    item.classList.add();
+  });
+  item.addEventListener('mouseout', () =>{
+    item.classList.remove();
+  })
+})
+
+function clickedRound(input){
+  let computerChoice = getComputerChoice()
+  let results = play(input, computerChoice);
+  roundResultContent.textContent = results;
+  roundResult.appendChild(roundResultContent);
+  checkScore(); // sees how scores are doing
+}
+
+function checkScore(){
+  if (yourScore === 5 || pcScore ===5){
+    if (yourScore === 5){
+      winnerContext.textContent = 'you WIN!';
     } else {
-      console.log(`You lose! ${computerChoice} beats ${userChoice}`);
+      winnerContext.textContent = 'you LOSE :(';
     }
+    winner.appendChild(winnerContext);
   }
-  return score;
 }
 
+// choices event listeners
+const rock = document.getElementById('rock');
+rock.addEventListener('click', ()=>{
+  clickedRound('rock')   //cannot invoke the callback right away need to do it after evenlistener is invoked, else the function will run right away
+});
+
+const paper = document.getElementById('paper');
+paper.addEventListener('click', ()=>{
+  clickedRound('paper')
+});
+
+const scissors = document.getElementById('scissors');
+scissors.addEventListener('click', ()=>{
+  clickedRound('scissors')
+});
+// can write a function to make this not verbose
 
 
+// MAIN SPACE
 let choices = ['rock', 'paper', 'scissors'];
-let scoreFinal = game()
-console.log(scoreFinal);
-// show on html figure it out later
-// document.getElementById("result").innerHTML = userChoice;
 
+//scores
+const currentScore = document.querySelector('#currentScore');
+const currentComputerScore = document.querySelector('#currentComputerScore');
 
-;
+//results
+const roundResult = document.querySelector('#result');
+const roundResultContent = document.createElement('h3');
 
+//winner
+const winner = document.querySelector('#winner');
+const winnerContext = document.createElement('h1');
+// const currentScoreContent = document.createElement('h3');
+let yourScore = 0;
+let pcScore = 0;
+currentScore.textContent = 0;
+currentComputerScore.textContent = 0;
